d0a06016a7713b4967125d80650f085dbcc1d44c
Sandenberg Ferreira Melo <77549672+sandenbergmelo@users.noreply.github.com>
2025-08-05 21:28:42 -0300

    Add shadcnui

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import tailwindcss from '@tailwindcss/vite'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
@@ -13,4 +14,9 @@ export default defineConfig({
       autoCodeSplitting: true,
     }),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
